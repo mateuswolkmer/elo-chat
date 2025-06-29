5905b4cf47cd1b27abc6a78d5e7efdfb989779a8
Mateus Wolkmer <mateuswsouza@gmail.com>
2025-06-29 01:13:18 -0300

    style: add transition to bot messages

--- src/components/MessagesList.tsx
+++ src/components/MessagesList.tsx
@@ -77,7 +77,18 @@ export const MessagesList: React.FC = () => {
                 )}
               >
                 <span className={twMerge(isThinking && "animate-pulse")}>
-                  {messageText}
+                  {!isAssistant
+                    ? messageText
+                    : messageText.split("").map((letter, index) => (
+                        <motion.span
+                          key={index}
+                          initial={{ opacity: 0 }}
+                          animate={{ opacity: 1 }}
+                          transition={{ duration: 0.1, delay: index * 0.005 }}
+                        >
+                          {letter}
+                        </motion.span>
+                      ))}
                 </span>
               </motion.p>
             );
